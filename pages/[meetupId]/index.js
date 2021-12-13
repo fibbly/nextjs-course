b0d9cad71646dfa9888596ea974ab096d93eb35b
Angel Perez <fibble@angels-mbp-2.lan>
2021-12-13 12:22:12 -0500

    feat: add meetup detail component with module css

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+	return (
+		<MeetupDetail
+			image="https://upload.wikimedia.org/wikipedia/commons/2/21/Sunset_gondola_Basilica_Della_Salute_.png"
+			address="sdfasfas"
+			description="dasasdfasdf"
+			title="First Meetup"
+		/>
+	);
+}
+
+export default MeetupDetails;
